dc1de35ee9af333718d9b1645933baa153dd9318
Muhammad Fau Zan Sabani <mfauzan18@student.ub.ac.id>
2026-06-28 22:42:51 +0700

    chore: add cloudinary configuration file for config caching

---
+++ config/cloudinary.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Cloudinary settings. Cloudinary is a cloud
+    | service that offers a solution to a web application's entire image
+    | management pipeline.
+    |
+    */
+
+    'cloudinary_url' => env('CLOUDINARY_URL'),
+
+];
